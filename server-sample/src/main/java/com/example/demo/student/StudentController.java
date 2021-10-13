071a25c9c8ae078b92e290f7fd2eb10bb9827c5b
KMS <epicblue@hanmail.net>
2021-10-13 21:25:02 +0900

    MyBatis Dependency 추가

--- server-sample/src/main/java/com/example/demo/student/StudentController.java
+++ server-sample/src/main/java/com/example/demo/student/StudentController.java
@@ -2,6 +2,7 @@ package com.example.demo.student;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path= "api/v1/student")
 public class StudentController {
 
-	private final StudentService studentService;
+    @Autowired
+	public StudentService studentService;
 	
-	public StudentController(StudentService studentService) {
-		this.studentService = studentService;
-	}
+
 	
     @GetMapping
     public List<Student> getStudents () {
