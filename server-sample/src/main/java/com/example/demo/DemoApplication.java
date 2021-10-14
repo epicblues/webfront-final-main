7e9cf13f6bd42c616a76aae82f0a0bdc079a4b97
KMS <epicblue@hanmail.net>
2021-10-14 12:33:42 +0900

    MyBatis configuration 추가 설정(수정 예정)

--- server-sample/src/main/java/com/example/demo/DemoApplication.java
+++ server-sample/src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@MapperScan(basePackageClasses = DemoApplication.class)
 @SpringBootApplication
 public class DemoApplication {
 
--- server-sample/src/main/java/com/example/demo/student/Student.java
+++ server-sample/src/main/java/com/example/demo/student/Student.java
@@ -2,6 +2,7 @@ package com.example.demo.student;
 
 import java.time.LocalDate;
 
+
 public class Student {
     private Long id;
     private String name;
