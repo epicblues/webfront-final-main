6081729dba34adb0bb544e102aef25ff7b414e55
KMS <epicblue@hanmail.net>
2021-10-15 09:30:01 +0900

    app small change

--- frontend/user/src/App.js
+++ frontend/user/src/App.js
@@ -1,26 +1,6 @@
 import "./App.css";
 
 function App() {
-  const counter = (function () {
-    // 사실상 class 에서 private member
-    let count = 0;
-    return {
-      // 사실상 class에서 public  member
-      increase() {
-        ++count;
-      },
-      decrease() {
-        --count;
-      },
-      show() {
-        console.log(count);
-      },
-    };
-  })();
-  counter.increase();
-  counter.increase();
-  counter.show();
-
   return <div className="App">Hello World</div>;
 }
 
