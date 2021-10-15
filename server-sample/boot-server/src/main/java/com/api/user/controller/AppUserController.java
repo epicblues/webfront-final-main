8b6d38a2108cc6713da8f34b66689e135fec6344
KMS <epicblue@hanmail.net>
2021-10-15 16:48:54 +0900

    Main Page Test

--- server-sample/boot-server/src/main/java/com/api/user/controller/AppUserController.java
+++ server-sample/boot-server/src/main/java/com/api/user/controller/AppUserController.java
@@ -2,6 +2,7 @@ package com.api.user.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,7 @@ public class AppUserController {
 	
 	@Autowired
 	AppUserDao appUserDao;
+	
 	
 	@ResponseBody
 	@GetMapping({"","/"})
---
+++ server-sample/boot-server/src/main/resources/static/js/join.js
@@ -0,0 +1,6 @@
+const body = document.querySelector("body");
+
+const h1 = document.createElement("h1");
+h1.innerHTML = "Test Static Assets";
+
+body.append(h1);
