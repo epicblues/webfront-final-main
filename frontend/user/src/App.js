08363139a25da4a6176879935675b628b00b9860
KMS <epicblue@hanmail.net>
2021-10-15 12:18:13 +0900

    user form initialized

--- frontend/user/src/App.js
+++ frontend/user/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <form>
+        id : <input type="email" name="email" id="email" />
+        <br />
+        password : <input type="password" name="password" id="password" />
+      </form>
     </div>
   );
 }
