8e9c5c355c3fc96afec74521129f74542903f0db
박지훈 <qkrw6@naver.com>
2021-10-16 21:18:31 +0900

    news-viewer

--- frontend/challenge 박지훈/react-movieapp/router-tutorial/src/Profiles.js
+++ frontend/challenge 박지훈/react-movieapp/router-tutorial/src/Profiles.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
+import { NavLink, Route } from "react-router-dom";
 import Profile from "./Profile";
 import WithRouterSample from "./WithRouterSample";
 const Profiles = () => {
+  const activeStyle = {
+    background: "black",
+    color: "white",
+  };
   return (
     <div>
       <h3>사용자 목록</h3>
       <ul>
         <li>
-          <Link to="/porfiles/velopart">velopart</Link>
+          <NavLink activeStyle={activeStyle} to="/porfiles/velopart">
+            velopart
+          </NavLink>
         </li>
         <li>
-          <Link to="/profiles/gildong">gildong</Link>
+          <NavLink activeStyle={activeStyle} to="/profiles/gildong">
+            gildong
+          </NavLink>
         </li>
       </ul>
 
