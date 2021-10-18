cbf0a7d73bde65ab62e8c0168e39333d2b6923aa
박지훈 <qkrw6@naver.com>
2021-10-18 16:48:17 +0900

    news api

--- frontend/challenge 박지훈/react-movieapp/news-viewer/src/App.js
+++ frontend/challenge 박지훈/react-movieapp/news-viewer/src/App.js
@@ -1,32 +1,8 @@
-import React, { useState } from "react";
-import axios from "axios";
-
+import React, { useState, useCallback } from "react";
+import { Route } from "react-router";
+import NewsPage from "./Pages/NewsPage";
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=c2a9cf57adbe4a5980f5050cde4442c9"
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <Route path="/:categotry?" component={NewsPage} />;
 };
 
 export default App;
--- frontend/challenge 박지훈/react-movieapp/news-viewer/src/Components/NewsList.js
+++ frontend/challenge 박지훈/react-movieapp/news-viewer/src/Components/NewsList.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
+import axios from "axios";
+import usePromise from "../lib/usePromise";
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
-  padding-bottomm: 3rem;
+  padding-bottom: 3rem;
   width: 768px;
   margin: 0 auto;
   margin-top: 2rem;
@@ -15,22 +17,35 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const smapleArticle = {
-  title: "제목",
-  description: "내용",
-  url: "https://google.com",
-  urlToImage: "https://via.placeholder.com/160",
-};
-const NewsList = () => {
+const NewsList = ({ category }) => {
+  const [loading, response, error] = usePromise(() => {
+    const query = category === "all" ? "" : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=c2a9cf57adbe4a5980f5050cde4442c9`
+    );
+  }, [category]);
+
+  // 대기중일 때
+  if (loading) {
+    return <NewsListBlock>대기중...</NewsListBlock>;
+  }
+  // 아직 response 값이 설정되지 않았을 때
+  if (!response) {
+    return null;
+  }
+
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
+
+  // response 값이 유효할 때
+  const { articles } = response.data;
   return (
     <NewsListBlock>
-      <NewsItem article={smapleArticle} />
-      <NewsItem article={smapleArticle} />
-      <NewsItem article={smapleArticle} />
-      <NewsItem article={smapleArticle} />
-      <NewsItem article={smapleArticle} />
-      <NewsItem article={smapleArticle} />
-      <NewsItem article={smapleArticle} />
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
     </NewsListBlock>
   );
 };
