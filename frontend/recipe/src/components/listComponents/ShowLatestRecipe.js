a1b5f9c7b951903b2e49185cab639b432fbf0d8a
kanglaeheon <kanglaeheon@gmail.com>
2021-10-18 21:46:32 +0900

    초기화면, 구조잡기

---
+++ frontend/recipe/src/components/listComponents/ShowLatestRecipe.js
@@ -0,0 +1,13 @@
+import ShowListOne from "./ShowListOne";
+function ShowLatestRecipe() {
+    return (
+        // 뒤에서 1번째 recipe item id값 인자로 넣어서 getListOne 함수실행
+        ShowListOne()
+        // 뒤에서 2번째 recipe item
+
+        // 뒤에서 3번째 recipe item
+        // 뒤에서 4번째 recipe item
+    );
+}
+
+export default ShowLatestRecipe;
