e094d546c2bbf0198e1a2e6c54187b6ecbfc43c4
박지훈 <qkrw6@naver.com>
2021-10-25 17:52:48 +0900

    챌린지 폼 작성

---
+++ main/components/challenge/ChallengeList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChallengeList = () => {
+  return <div>ChallengeList Test</div>;
+};
+
+export default ChallengeList;
---
+++ main/components/challenge/ChallengeMainList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChallengeMainList = () => {
+  return <div>메인 챌린지 리스트</div>;
+};
+
+export default ChallengeMainList;
