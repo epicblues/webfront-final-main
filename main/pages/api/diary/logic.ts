85296dc5f2c69c0729050194ca3854258641a3a9
KMS <epicblue@hanmail.net>
2021-10-29 15:22:39 +0900

    MongoDB CRUD 연습

---
+++ main/pages/api/diary/logic.ts
@@ -0,0 +1,9 @@
+import { NextApiHandler } from "next";
+
+// basic logic
+// 다이어리 데이터 일 단위로 저장.(중복 불가능 일종의 index)
+// 그냥 Date 형태로 저장해야 하는가? -> Date와 일 단위 포함 저장
+
+const handler: NextApiHandler = (req, res) => {};
+
+export default handler;
