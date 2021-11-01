f71fc36d9984d435c5c9df47d8fb3aab54d1e660
kanglaeheon <kanglaeheon@gmail.com>
2021-11-01 15:58:04 +0900

    recipe/create의 IngredientForm <-> food_db 통신 적용

--- main/components/recipe/createRecipe/IngredientForm.js
+++ main/components/recipe/createRecipe/IngredientForm.js
@@ -1,69 +1,59 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-import { NextApiResponse, NextApiRequest } from 'next'
-
-const initialValues = {
-    igr_array: []
-}
-
-async function getFoodDataByNameWithin() {
-    try {
-        const res = await axios.get(`api/food?name=${foodName}`);
-        console.log(res)
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-// 동기처리 버전
-// const getFoodDataByName = (foodName) => {
-//     axios.get(`api/food?name=${foodName}`)
-//     .then(function(res) {
-//         console.log(res)
-//     })
-//     .catch(function(error) {
-//         console.log(error)
-//     })
-
 const IngredientForm = () => {
-    const { register, watch } = useForm();
-    let searchVal = watch();
-    console.log(searchVal);
-
-
+    const [allData, setAllData] = useState([]);
+    const [filteredData, setFilteredData] = useState(allData);
+    const handleSearch = async (event) => {
+        const value = event.target.value;
+        const { data } = await axios.get("/api/food/" + value);
+        console.log(data);
+        setFilteredData(data);
+    };
     
-    //  axios JSON 요청 및 응답 받아와서 리스트 배열 뿌리기 로직 구현 필요
-    // localhost/3000/api/recipe/food?searchVal=value
-
-    //  로컬스토리지 개념 이해 및 테스트
-
-    //  igr_array에 food.name 값 검색하는 쿼리문 예시
-    //  db.getCollection('food').find({"name" : { $regex: "^귀리" } })
+    useEffect(() => {
+        axios("https://jsonplaceholder.typicode.com/albums/1/photos")
+          .then((response) => {
+            console.log(response.data);
+            setAllData(response.data);
+            setFilteredData(response.data);
+          })
+          .catch((error) => {
+            console.log("Error getting fake data: " + error);
+          });
+    }, []);
 
     return (
         <div>
-            <form>
-                <label>재료</label>
-                <input 
-                    type="text"
-                    placeholder="예)양파"
-                    {...register("name")}
-                />
+            <label>재료</label>
+            <input 
+                type="text"
+                placeholder="예)양파"
+                onChange={(event) => handleSearch(event)}
+            />
+            <div>
+                {filteredData.map((value, index) => {
+                    return (
+                        // 검색 리스트 출력
+                        <div key={value.id}>
+                            {value.name}
+                            {value.mfr}
+                        </div>
+                    );
+                })}
+            </div>
 
-                <label>양</label>
-                <input type="text" placeholder="예)100" />
+            <label>양</label>
+            <input type="text" placeholder="예)100" />
 
-                <label>단위</label>
-                <select>
-                    <option value="g">g</option>
-                    <option value="ml">ml</option>
-                </select>
-                <button type="button">추가</button>
-            </form>
+            <label>단위</label>
+            <select>
+                <option value="g">g</option>
+                <option value="ml">ml</option>
+            </select>
+            <button type="button">추가</button>
         </div>
-    )
+    );
 }
 
 export default IngredientForm
