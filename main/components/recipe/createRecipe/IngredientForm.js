b0a07e13e000c48c747c6b7011245a4bcf2d5de8
kanglaeheon <kanglaeheon@gmail.com>
2021-11-01 10:05:38 +0900

    food_db_eng 요청 API 작성중

--- main/components/recipe/createRecipe/IngredientForm.js
+++ main/components/recipe/createRecipe/IngredientForm.js
@@ -1,14 +1,46 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import axios from 'axios'
+
+import { NextApiResponse, NextApiRequest } from 'next'
 
 const initialValues = {
     igr_array: []
 }
 
+async function getFoodDataByNameWithin() {
+    try {
+        const res = await axios.get(`api/food?name=${foodName}`);
+        console.log(res)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// 동기처리 버전
+// const getFoodDataByName = (foodName) => {
+//     axios.get(`api/food?name=${foodName}`)
+//     .then(function(res) {
+//         console.log(res)
+//     })
+//     .catch(function(error) {
+//         console.log(error)
+//     })
+
 const IngredientForm = () => {
     const { register, watch } = useForm();
     let searchVal = watch();
-    console.log(searchVal)
+    console.log(searchVal);
+
+
+    
+    //  axios JSON 요청 및 응답 받아와서 리스트 배열 뿌리기 로직 구현 필요
+    // localhost/3000/api/recipe/food?searchVal=value
+
+    //  로컬스토리지 개념 이해 및 테스트
+
+    //  igr_array에 food.name 값 검색하는 쿼리문 예시
+    //  db.getCollection('food').find({"name" : { $regex: "^귀리" } })
 
     return (
         <div>
@@ -17,7 +49,7 @@ const IngredientForm = () => {
                 <input 
                     type="text"
                     placeholder="예)양파"
-                    {...register("InputVal")}
+                    {...register("name")}
                 />
 
                 <label>양</label>
