76ae688f9d8ab1c95a88de404bfd76c59e309c75
kanglaeheon <kanglaeheon@gmail.com>
2021-11-05 19:10:06 +0900

    recipe/list 데이터 연결 완료, recipe/card/[post_id] 개별 페이지 작성 중

--- main/pages/recipe/card/[post_no]/index.js
+++ main/pages/recipe/card/[post_no]/index.js
@@ -8,18 +8,14 @@ const index = ({user, recipe}) => {
   return (
     <div>
       <div>
-      <img src={recipe.steps[0].image_url} />
-        {/* 상단 레시피 인트로 이미지, 썸네일과 동일 */}
-        {/* <img id="rcp_thumb" src={`../${card.rcp_thumb_url}`} /> */}
-        {/* 카테고리 중간 사이즈 폰트 */}
-        {/* <p id="rcp_category">{card.rcp_category}</p> */}
-        {/* 레시피 이름 */}
-        {/* <p id="rcp_main_title">{card.rcp_main_title}</p> */}
-        {/* 레시피 설명 */}
-        {/* <p id="rcp_desc">{card.rcp_desc}</p> */}
-        {/* 레시피 재료 */}
-        {/* 영양성분표 Modal */}
-        {/* <input type="button" value="영양성분보기" /> */}
+        <img src={recipe.steps[0].image_url} />
+        <h2>{recipe.title}</h2>
+        <p>{recipe.desc}</p>
+        <p>등록일: {recipe.upload_date}</p>
+      </div>
+      <div>
+        <h3>레시피 재료</h3>
+        
       </div>
     </div>
   );
