224b5efd4acdb24c6e2ab5ef657af0c321cdf474
KMS <epicblue@hanmail.net>
2021-11-09 16:52:07 +0900

    Diary Main Page 일부 구현

--- main/pages/diary/products.js
+++ main/pages/diary/products.js
@@ -1,33 +1,32 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from "react";
 import axios from "axios";
-import { Button, Header, Modal } from 'semantic-ui-react';
+import { Button, Header, Modal } from "semantic-ui-react";
 
-export default function Products({ setCart, cart }) {
+export default function Products({ setCart, cart, diary, setDiary, type }) {
   const inputRef = useRef();
   const [products] = useState([]);
   const [selectedData, setSelectedData] = useState([]);
 
   const [style, setStyle] = useState();
-  
+
   const changeStyle = () => {
-    return <i className='check circle icon'></i>
-  }
+    return <i className="check circle icon"></i>;
+  };
 
   const addToCart = (value) => {
-    let newCart = [...cart];
-    let itemInCart = newCart.find(
-      (item) => value.name === item.name
-    );
-    if (itemInCart) {
-      itemInCart.quantity++;
-    } else {
-      itemInCart = {
-        ...value,
-        quantity: 1,
-      };
-      newCart.push(itemInCart);
-    }
-    setCart(newCart);
+    const prevMeal = diary.meals[type];
+
+    prevMeal.foods.push(value);
+    prevMeal.calories += value.kcal;
+    prevMeal.fat += value.fat;
+    prevMeal.carbs += value.carbs;
+    prevMeal.protein += value.prot;
+    const currentMeals = diary.meals;
+    currentMeals.splice(type, 1, prevMeal);
+    setDiary({
+      ...diary,
+      meals: currentMeals,
+    });
   };
 
   // 검색필터
@@ -40,57 +39,64 @@ export default function Products({ setCart, cart }) {
     setFilteredData(data);
   };
 
-  const [open, setOpen] = React.useState(false)
+  const [open, setOpen] = React.useState(false);
 
   return (
     <>
-      <div className="ui fluid icon input" style={{padding: '0 16px'}}>
+      <div className="ui fluid icon input" style={{ padding: "0 16px" }}>
         <input
           type="text"
           placeholder="음식 검색하기"
           onChange={(event) => handleSearch(event)}
         />
-        <i className="search icon" style={{right: 16}}></i>
+        <i className="search icon" style={{ right: 16 }}></i>
       </div>
 
       <div className="ui middle aligned divided list" style={{ padding: 10 }}>
         {filteredData.map((value, index) => {
           return (
             // 검색 리스트 출력
-            <div className="item"
-                  key={value.id}
-                  style={{padding: '8px'}}
-            >
-              <div style={{
-                      textAlign: "left",
-                      display: 'grid',
-                      gridTemplateColumns: '9.5fr 0.5fr'}}
+            <div className="item" key={value.id} style={{ padding: "8px" }}>
+              <div
+                style={{
+                  textAlign: "left",
+                  display: "grid",
+                  gridTemplateColumns: "9.5fr 0.5fr",
+                }}
               >
                 <div>
-                    <Modal
-                      onClose={() => setOpen(false)}
-                      onOpen={() => setOpen(true)}
-                      open={open}
-                      trigger={
-                        <div className="content"
-                              style={{
-                                display: "flex",
-                                justifyContent: "space-between",
-                              }}
-                        >
-                            <div className="header">
-                              {value.name}
-                              <div className="description">{value.mfr} / {value.serve}gram</div>
-                            </div>
-                            <div className='right floated' style={{margin: '8px 10px 0 0'}}>{value.kcal}Kcal</div>
+                  <Modal
+                    onClose={() => setOpen(false)}
+                    onOpen={() => setOpen(true)}
+                    open={open}
+                    trigger={
+                      <div
+                        className="content"
+                        style={{
+                          display: "flex",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <div className="header">
+                          {value.name}
+                          <div className="description">
+                            {value.mfr} / {value.serve}gram
+                          </div>
                         </div>
-                      }
-                    >
-                      <Modal.Header>선택한 음식</Modal.Header>
-                      <Modal.Content>
-                          <Modal.Description>
-                              <Header>영양정보</Header>
-                              {/* {selectedData.map((value, index) => {
+                        <div
+                          className="right floated"
+                          style={{ margin: "8px 10px 0 0" }}
+                        >
+                          {value.kcal}Kcal
+                        </div>
+                      </div>
+                    }
+                  >
+                    <Modal.Header>선택한 음식</Modal.Header>
+                    <Modal.Content>
+                      <Modal.Description>
+                        <Header>영양정보</Header>
+                        {/* {selectedData.map((value, index) => {
                                 return (
                                     <>
                                     <div key={index}>
@@ -114,33 +120,40 @@ export default function Products({ setCart, cart }) {
                                     </>
                                 );
                             })} */}
-                          </Modal.Description>
-                      </Modal.Content>
-                      <Modal.Actions>
-                          <Button color='black' onClick={() => setOpen(false)}>
-                          취소
-                          </Button>
-                          <Button
-                          content="확인"
-                          labelPosition='right'
-                          icon='checkmark'
-                          onClick={() => {setOpen(false); addToCart(value)}}
-                          positive
-                          />
-                      </Modal.Actions>
-                    </Modal>
+                      </Modal.Description>
+                    </Modal.Content>
+                    <Modal.Actions>
+                      <Button color="black" onClick={() => setOpen(false)}>
+                        취소
+                      </Button>
+                      <Button
+                        content="확인"
+                        labelPosition="right"
+                        icon="checkmark"
+                        onClick={() => {
+                          setOpen(false);
+                          addToCart(value);
+                        }}
+                        positive
+                      />
+                    </Modal.Actions>
+                  </Modal>
                 </div>
-                <i className='teal plus circle icon right floated'
-                    onClick={(e) => {
-                      addToCart(value);
-                      e.currentTarget.className='green check circle icon right floated';
-                      const targetReverse = (target) => () => {
-                        target.className = 'teal plus circle icon right floated'
-                      }
-                      setTimeout( targetReverse(e.currentTarget),1000)}}
-                    style={{marginTop: 8}}
-                >
-                </i>
+                <i
+                  className="teal plus circle icon right floated"
+                  onClick={(e) => {
+                    console.log(value);
+                    addToCart(value);
+
+                    e.currentTarget.className =
+                      "green check circle icon right floated";
+                    const targetReverse = (target) => () => {
+                      target.className = "teal plus circle icon right floated";
+                    };
+                    setTimeout(targetReverse(e.currentTarget), 1000);
+                  }}
+                  style={{ marginTop: 8 }}
+                ></i>
               </div>
             </div>
           );
