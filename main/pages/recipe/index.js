7f62a840d034bdf1169eed5bac50e5892b1e8b82
kanglaeheon <kanglaeheon@gmail.com>
2021-11-17 10:11:51 +0900

    /recipe 인기 레시피, 최신 레시피 구현

--- main/pages/recipe/index.js
+++ main/pages/recipe/index.js
@@ -11,9 +11,32 @@ import Search from "../../components/recipe/index/Search";
 // CSS
 import recipeListStyles from "../../styles/RecipeList.module.css";
 
-const Index = (user, filteredLatestRecipes, filteredHitRecipes) => {
-  const [hitRecipes, setHitRecipes] = useState([filteredHitRecipes]);
+const Index = ({ user, filteredLatestRecipes, filteredHitRecipes }) => {
   const [showCategories, setShowCategories] = useState(true);
+
+  // 카테고리 값(Int)에 맞는 카테고리명(String) 표시 함수
+  function renderSwitchCategory(param) {
+    switch (param) {
+      case "soup":
+        return "국/탕/찌개";
+      case "grill":
+        return "구이";
+      case "noodle":
+        return "면/파스타";
+      case "rice":
+        return "밥/볶음밥";
+      case "side":
+        return "반찬";
+      case "kimchi":
+        return "김치";
+      case "dessert":
+        return "디저트";
+      case "etc":
+        return "기타";
+      default:
+        return "몰라용";
+    }
+  }
 
   return (
     <div>
@@ -51,7 +74,7 @@ const Index = (user, filteredLatestRecipes, filteredHitRecipes) => {
       {/* 인기 레시피 */}
       <h3>인기 레시피</h3>
       <ul className={recipeListStyles.cards}>
-        {hitRecipes.map((card, index) => {
+        {filteredLatestRecipes.map((card, index) => {
           return (
             <li key={card._id}>
               <Link
@@ -81,9 +104,40 @@ const Index = (user, filteredLatestRecipes, filteredHitRecipes) => {
           );
         })}
       </ul>
+
       {/* 최신 레시피 */}
       <h3>최신 레시피</h3>
-      {/* <ShowLatestCard rcpData={_rcpData} /> */}
+      <ul className={recipeListStyles.cards}>
+        {filteredHitRecipes.map((card, index) => {
+          return (
+            <li key={card._id}>
+              <Link
+                href={{
+                  pathname: `/recipe/card/${card._id}`,
+                }}
+                as={`/recipe/card/${card._id}`}
+                passHref
+              >
+                <a>
+                  <Image
+                    src={
+                      process.env.NEXT_PUBLIC_STATIC_SERVER_URL +
+                      card.steps.slice(-1)[0].image_url
+                    }
+                    width={100}
+                    height={100}
+                    alt="thumbnail image"
+                  />
+                  <p>{card.title}</p>
+                  <p>카테고리: {renderSwitchCategory(card.category)}</p>
+                  <p>작성자: {card.author[0].name}</p>
+                  <p>조회수: {card.hit}</p>
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
@@ -131,6 +185,6 @@ export const getServerSideProps = async (ctx) => {
     .toArray();
   const filteredHitRecipes = JSON.parse(JSON.stringify(data2));
 
-  return { props: { user, latestRecipes, hitRecipes } };
+  return { props: { user, filteredLatestRecipes, filteredHitRecipes } };
 };
 export default Index;
