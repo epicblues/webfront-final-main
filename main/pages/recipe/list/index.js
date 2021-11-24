a800b747f4cca0021243fef882acf8ab7e268036
kanglaeheon <kanglaeheon@gmail.com>
2021-11-24 17:58:14 +0900

    무한스크롤 부분적 적용, 추가 데이터 없을 경우 표시되는 메시지 수정 필요

--- main/pages/recipe/list/index.js
+++ main/pages/recipe/list/index.js
@@ -1,11 +1,13 @@
 // page component '/recipe/list'
 // 모든 레시피를 보여주는 리스트
 
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { getUserOrRedirect } from "../../../util/auth";
 import clientPromise from "../../../util/mongodb";
+import axios from "axios";
+import { debounce } from "../../../util/axios";
 
 import Categories from "../../../components/recipe/index/Categories";
 import Search from "../../../components/recipe/index/Search";
@@ -13,7 +15,7 @@ import Search from "../../../components/recipe/index/Search";
 //  CSS
 import recipeListStyles from "../../../styles/RecipeList.module.css";
 import { parseDocumentToObject } from "../../../util/date";
-import GoBackward from "../../../components/GoBackward";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Index = ({ user, recipes }) => {
   //  카테고리 값(Int)에 맞는 카테고리명(String) 표시 함수
@@ -40,45 +42,63 @@ const Index = ({ user, recipes }) => {
     }
   }
 
+  const [hasMore, setHasMore] = useState(true);
+  const [recipeList, setRecipeList] = useState(recipes);
+  const [recipeCounter, setRecipeCounter] = useState(4);
+
+  const getMoreRecipes = debounce(async () => {
+    const { data } = await axios.get("/api/recipe/more/" + recipeCounter);
+    setRecipeList([...recipeList, ...data]);
+    console.log("ONSCROLL!");
+    setRecipeCounter(recipeCounter + 4);
+  }, 500);
+
   return (
     <div>
-      <GoBackward />
       <Search />
       <Categories />
-      <h1>카테고리 : 전체</h1>
-      <div>
-        <ul className={recipeListStyles.cards}>
-          {recipes.map((card, index) => {
-            return (
-              <li key={card._id}>
-                <Link
-                  href={{
-                    pathname: `/recipe/card/${card._id}`,
-                  }}
-                  as={`/recipe/card/${card._id}`}
-                  passHref
-                >
-                  <a>
-                    <Image
-                      src={
-                        process.env.NEXT_PUBLIC_STATIC_SERVER_URL +
-                        card.steps.slice(-1)[0].image_url
-                      }
-                      width={100}
-                      height={100}
-                      alt="thumbnail image"
-                    />
-                    <p>{card.title}</p>
-                    <p>카테고리: {renderSwitchCategory(card.category)}</p>
-                    <p>작성자: {card.author[0].name}</p>
-                    <p>조회수: {card.hit}</p>
-                  </a>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <InfiniteScroll
+        dataLength={recipes.length}
+        next={getMoreRecipes}
+        hasMore={hasMore}
+        loader={<h3> Loading... </h3>}
+        endMessage={<h4>Nothing more to show</h4>}
+      >
+        <h1>카테고리 : 전체</h1>
+        <div>
+          <ul className={recipeListStyles.cards}>
+            {recipeList.map((card, index) => {
+              return (
+                <li key={card._id}>
+                  <Link
+                    href={{
+                      pathname: `/recipe/card/${card._id}`,
+                    }}
+                    as={`/recipe/card/${card._id}`}
+                    passHref
+                  >
+                    <a>
+                      <Image
+                        src={
+                          process.env.NEXT_PUBLIC_STATIC_SERVER_URL +
+                          card.steps.slice(-1)[0].image_url
+                        }
+                        width={100}
+                        height={100}
+                        alt="thumbnail image"
+                      />
+                      <p>{card.title}</p>
+                      <p>카테고리: {renderSwitchCategory(card.category)}</p>
+                      <p>작성자: {card.author[0].name}</p>
+                      <p>조회수: {card.hit}</p>
+                    </a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </InfiniteScroll>
     </div>
   );
 };
@@ -101,7 +121,8 @@ export const getServerSideProps = async (ctx) => {
         },
       },
     ])
-    .limit(9)
+    .sort({ upload_date: -1 })
+    .limit(4)
     .toArray();
   const recipes = parseDocumentToObject(data);
 
