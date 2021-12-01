22a4427e7d90e1537f3cc48a700ba98508b253ea
kanglaeheon <kanglaeheon@gmail.com>
2021-12-01 17:45:11 +0900

    _app.tsx <Head> 컴포넌트 이용 자동 확대기능 억제

--- main/pages/_app.tsx
+++ main/pages/_app.tsx
@@ -3,12 +3,16 @@ import type { AppProps } from 'next/app'
 import 'semantic-ui-css/semantic.min.css'
 
 import Layout from '../components/Layout'
+import Head from 'next/head'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <Layout pageProps={{ ...pageProps }}>
+      <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
