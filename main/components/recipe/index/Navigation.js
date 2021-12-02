a8daa571b9adb067cf83f858e33abccfaa9475ce
kanglaeheon <kanglaeheon@gmail.com>
2021-12-02 13:42:12 +0900

    recipe/index CSS 최적화, Swipe 기능 최적화

---
+++ main/components/recipe/index/Navigation.js
@@ -0,0 +1,8 @@
+import React from "react";
+import navigationStyles from "../../../styles/recipe/Navigation.module.css";
+
+const Navigation = () => {
+  return <div className={navigationStyles.container}>상단내비</div>;
+};
+
+export default Navigation;
--- main/components/recipe/list/CardsSwiper.js
+++ main/components/recipe/list/CardsSwiper.js
@@ -10,6 +10,7 @@ import "swiper/swiper.min.css";
 import cardsSwiperStyles from "../../../styles/recipe/CardsSwiper.module.css";
 
 // ICON
+import ci from "../../../public/static/logos/icon_check.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 
@@ -40,7 +41,11 @@ const CardsSwiper = ({ filteredHitRecipes }) => {
     }
   }
   const swiperParams = {
+    speed: 200,
     slidesPerView: 1.3,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 30,
     onSwiper: setSwiper,
     onSlideChange: (e) => setMainImageIndex(e.activeIndex),
   };
@@ -54,66 +59,74 @@ const CardsSwiper = ({ filteredHitRecipes }) => {
       {filteredHitRecipes.map((card, index) => {
         return (
           <SwiperSlide key={card._id} className={cardsSwiperStyles.card}>
-            <Link
-              href={{
-                pathname: `/recipe/card/${card._id}`,
-              }}
-              as={`/recipe/card/${card._id}`}
-              passHref
-            >
-              <a>
-                {/* 카드 헤더 (이미지) */}
-                <div className={cardsSwiperStyles.cardHeader}>
-                  <Image
-                    className={cardsSwiperStyles.cardHeaderImage}
-                    src={
-                      process.env.NEXT_PUBLIC_STATIC_SERVER_URL +
-                      card.steps.slice(-1)[0].image_url
-                    }
-                    layout="fill"
-                    objectFit="cover"
-                    objectPosition="top"
-                    alt="thumbnail image"
-                  />
+            {/* 카드 헤더 (이미지) */}
+            <div className={cardsSwiperStyles.cardHeader}>
+              <Image
+                className={cardsSwiperStyles.cardHeaderImage}
+                src={
+                  process.env.NEXT_PUBLIC_STATIC_SERVER_URL +
+                  card.steps.slice(-1)[0].image_url
+                }
+                layout="fill"
+                objectFit="cover"
+                objectPosition="top"
+                alt="thumbnail image"
+              />
+            </div>
+            {/* 카드 바디 */}
+            <div className={cardsSwiperStyles.cardBodyWrapper}>
+              {/* 카드 바디 헤더 */}
+              <div className={cardsSwiperStyles.cardBodyHeader}>
+                <div className={cardsSwiperStyles.hot}>
+                  <div className={cardsSwiperStyles.iWrapper}>
+                    <Image objectFit="fill" objectFit="contain" src={ci} />
+                  </div>
+                  <strong>요즘 뜨는 레시피</strong>
                 </div>
-                {/* 카드 바디 */}
-                <div className={cardsSwiperStyles.cardBodyMain}>
-                  {/* 카드 바디 헤더 */}
-                  <div className={cardsSwiperStyles.cardBodyHeader}>
-                    <div className={cardsSwiperStyles.hot}>
-                      /요즘 뜨는 레시피/
-                    </div>
-                    <h1 className={cardsSwiperStyles.h1}>{card.title}</h1>
-                    <p className={cardsSwiperStyles.cardBodyCategory}>
-                      #{renderSwitchCategory(card.category)}
-                    </p>
-                    <p className={cardsSwiperStyles.cardBodyAuthor}>
-                      작성자: {card.author[0].name}
-                    </p>
-                  </div>
+                <h1 className={cardsSwiperStyles.h1}>{card.title}</h1>
+                <p className={cardsSwiperStyles.cardBodyAuthor}>
+                  <strong>작성자</strong> | {card.author[0].name}
+                </p>
+                <div className={cardsSwiperStyles.cardBodyCategory}>
+                  <p>#{renderSwitchCategory(card.category)}</p>
+                </div>
+              </div>
 
-                  {/* 카드 바디 본문 */}
-                  <div className={cardsSwiperStyles.cardBodyMain}>
-                    <p className={cardsSwiperStyles.cardBodyDesc}>
-                      {card.desc}
-                    </p>
-                  </div>
-
-                  {/* 카드 바디 푸터 */}
-                  <div className={cardsSwiperStyles.cardBodyFooter}>
-                    <hr className={cardsSwiperStyles.hr} />
+              {/* 카드 바디 본문 */}
+              <div className={cardsSwiperStyles.cardBodyMain}>
+                <p className={cardsSwiperStyles.cardBodyDesc}>
+                  <Link
+                    href={{
+                      pathname: `/recipe/card/${card._id}`,
+                    }}
+                    as={`/recipe/card/${card._id}`}
+                    passHref
+                  >
+                    <a>{card.desc}</a>
+                  </Link>
+                </p>
+              </div>
+              {/* 카드 바디 푸터 */}
+              <div className={cardsSwiperStyles.cardBodyFooter}>
+                <div className={cardsSwiperStyles.hr}></div>
+                <div className={cardsSwiperStyles.textWrapper}>
+                  <span>
                     <FontAwesomeIcon
                       className={cardsSwiperStyles.cardIconHit}
                       icon={faEye}
                     />
-                    <span>조회 {card.hit}회</span>
-                    <span className={cardsSwiperStyles.cardUploadDate}>
-                      {card.upload_date.slice(0, -14)}
-                    </span>
-                  </div>
+                    조회{" "}
+                    <strong className={cardsSwiperStyles.hitSpan}>
+                      {card.hit}
+                    </strong>
+                    회
+                  </span>
+                  <span className={cardsSwiperStyles.cardUploadDate}>
+                    {card.upload_date.slice(0, -14)}
+                  </span>
                 </div>
-              </a>
-            </Link>
+              </div>
+            </div>
           </SwiperSlide>
         );
       })}
