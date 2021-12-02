799341df10f13e3452667a3b6c1df090fa1bb11d
KMS <epicblue@hanmail.net>
2021-12-02 21:09:47 +0900

    로그인 페이지 애니메이션 구현

--- main/components/user/Intro.tsx
+++ main/components/user/Intro.tsx
@@ -1,7 +1,7 @@
 import React, { CSSProperties } from 'react'
-import { Button } from 'semantic-ui-react'
+import animation from '../../styles/user/animation.module.css'
 
-const Intro = ({ handleClick }: { handleClick: Function }) => {
+const Intro = ({ handleClick, loginMode }: { handleClick: Function, loginMode: boolean }) => {
 
   const container: CSSProperties = {
     display: 'flex',
@@ -9,8 +9,9 @@ const Intro = ({ handleClick }: { handleClick: Function }) => {
     justifyContent: "space-between",
     alignItems: "center",
     margin: "20px",
-    minHeight: "50vh"
-
+    height: "70vh",
+    transition: "all 0.5s",
+    transform: loginMode ? "translate(-110%,-60vh)" : 'translate(0%,-60vh)'
 
   }
 
@@ -27,22 +28,22 @@ const Intro = ({ handleClick }: { handleClick: Function }) => {
   return (
     <div style={container}>
       <h1>요건 다 내꺼</h1>
-      <h3>
+      <h2>
         요리와 건강을 모두 챙겨봅시다!
-      </h3>
+      </h2>
 
       <div style={item}>
         <h3>Recipe</h3>
-        <div>🍖 레시피를 작성하고 회원들과 공유할 수 있습니다.</div>
+        <div style={{ fontSize: "1.1em" }}>🍖 레시피를 작성하고 회원들과 공유할 수 있습니다.</div>
         <h3>Diary</h3>
-        <div>💪 먹은 음식의 영양 성분을 기록할 수 있습니다. </div>
+        <div style={{ fontSize: "1.1em" }}>💪 먹은 음식의 영양 성분을 기록할 수 있습니다. </div>
         <h3>Challenge</h3>
-        <div>⌛️ &nbsp;&nbsp;보다 능동적인 건강관리를 할 수 있습니다.</div>
+        <div style={{ fontSize: "1.1em" }}>⌛️ &nbsp;&nbsp;보다 능동적인 건강관리를 할 수 있습니다.</div>
       </div>
 
 
 
-      <button style={{ paddingLeft: "5px", paddingRight: "10px", backgroundColor: "#00b5ad", fontWeight: "bolder", borderRadius: "5px", color: "white", boxShadow: "none", border: "none", alignSelf: "stretch", height: "30px", marginTop: "10px" }} onClick={() => { handleClick() }}>참여하기</button>
+      <button style={{ paddingLeft: "5px", paddingRight: "10px", backgroundColor: "#00b5ad", fontWeight: "bolder", borderRadius: "5px", color: "white", boxShadow: "none", border: "none", alignSelf: "stretch", height: "40px", marginTop: "10px", fontSize: "1.2em" }} onClick={() => { handleClick() }}>참여하기</button>
     </div>
   )
 }
