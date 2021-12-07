0607d6eec3b675d8c3f588e153a75f5e69855ef4
박지훈 <qkrw6@naver.com>
2021-12-07 15:54:12 +0900

    test

--- main/pages/challenge/index.js
+++ main/pages/challenge/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Link from "next/link";
-import "semantic-ui-css/semantic.min.css";
+import axios from "axios";
+import clientPromise from "../../util/mongodb";
 import { getUserOrRedirect } from "../../util/auth";
-import { Button, List } from "semantic-ui-react";
+//component
 import MyChallenge from "../../components/challenge/Main/MyChallenge";
 import ProgressBar from "../../components/challenge/Main/ProgressBar";
-import clientPromise from "../../util/mongodb";
+//css
+import "semantic-ui-css/semantic.min.css";
 import ListStyle from "../../styles/challenge/List.module.css";
 import ButtonStyle from "../../styles/challenge/Button.module.css";
 import ChallengeStyle from "../../styles/challenge/Challenge.module.css";
-import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
 const index = ({ challenges, user }) => {
   const participatedChallenges = challenges.filter(
     (challenge) =>
@@ -18,141 +22,152 @@ const index = ({ challenges, user }) => {
   );
   return (
     <>
-      <div>
-        <Link passHref href="/challenge/mainlist">
-          <p>전체</p>
-        </Link>
-      </div>
-      <div>
-        <Link passHref href="/challenge/mainlist">
-          <p></p>
-        </Link>
-      </div>
-      <br />
+      <div style={{ backgroundColor: "#fff0f0" }}>
+        <div>
+          <Link passHref href="/challenge/mainlist">
+            <p>전체</p>
+          </Link>
+        </div>
+        <div>
+          <Link passHref href="/challenge/mainlist">
+            <p></p>
+          </Link>
+        </div>
+        <br />
 
-      <div className={ChallengeStyle.container2}>
-        <h2>참여한 챌린지</h2>
-        <MyChallenge
-          challenges={challenges}
-          user={user}
-          key={challenges.id}
-        ></MyChallenge>
-        {challenges.map((challenge) => {
-          return (
-            <>
-              {!(challenge.participants.indexOf(user.id) === -1) ? (
-                <>
-                  <div className={ChallengeStyle.item}>
-                    <ul className={ListStyle.ul}>
-                      <li className={ListStyle.li}>
-                        <Link
-                          passHref
-                          href={"/challenge/list/" + challenge._id}
-                        >
-                          <a>
-                            {" "}
-                            <li key={challenge.id}>
-                              챌린지 명:{challenge.title}
-                            </li>
-                          </a>
-                        </Link>
-                        <li key={challenge.id}>
-                          작성자:{challenge.author[0].name}
+        <div className={ChallengeStyle.container2}>
+          <h2>참여한 챌린지</h2>
+          <MyChallenge
+            challenges={challenges}
+            user={user}
+            key={challenges.id}
+          ></MyChallenge>
+          {challenges.map((challenge) => {
+            return (
+              <>
+                {!(challenge.participants.indexOf(user.id) === -1) ? (
+                  <>
+                    <div className={ChallengeStyle.item}>
+                      <div>
+                        <h4> 참가자 수:{challenge.participants.length}명</h4>
+                      </div>
+                      <ul className={ListStyle.ul}>
+                        <li className={ListStyle.li}>
+                          <Link
+                            passHref
+                            href={"/challenge/list/" + challenge._id}
+                          >
+                            <a>
+                              {" "}
+                              <li key={challenge.id}>
+                                챌린지 명:{challenge.title}
+                              </li>
+                            </a>
+                          </Link>
+                          <li key={challenge.id}>
+                            작성자:{challenge.author[0].name}
+                          </li>
+                          <li key={challenge.id}>
+                            챌린지 기한:
+                            {new Date(challenge.startDate).toLocaleDateString()}
+                            ~ {new Date(challenge.endDate).toLocaleDateString()}
+                          </li>
+                          {challenge.type === "diet" ? (
+                            <li key={challenge.id}>챌린지 종류: 다이어트</li>
+                          ) : (
+                            <li key={challenge.id}>챌린지 종류: 레시피</li>
+                          )}
                         </li>
-                        <li key={challenge.id}>
-                          참가자 수:{challenge.participants.length}명
-                        </li>
-                        <li key={challenge.id}>
-                          챌린지 기한:
-                          {new Date(
-                            challenge.startDate
-                          ).toLocaleDateString()}~{" "}
-                          {new Date(challenge.endDate).toLocaleDateString()}
-                        </li>
-                        {challenge.type === "diet" ? (
-                          <li key={challenge.id}>챌린지 종류: 다이어트</li>
-                        ) : (
-                          <li key={challenge.id}>챌린지 종류: 레시피</li>
-                        )}
-                      </li>
-                    </ul>
-                    <div style={{ display: "flex", justifyContent: "center" }}>
-                      <button
-                        className={ButtonStyle.button}
-                        onClick={async (event) => {
-                          const { data: result } = await axios.post(
-                            "/api/challenge/validate",
-                            challenge
-                          );
-                          console.log(result);
-                          const button = event.target;
-
-                          button.disabled = true;
-                          if (Array.isArray(result)) {
-                            // 실패했다.
-                            button.textContent = "진행중!";
-                            button.style.color = "white";
-                            setInterval(
-                              (button) => {
-                                button.style.display = "none";
-                              },
-                              2000,
-                              button
-                            );
-                            const progressBar = button.nextElementSibling;
-                            if (progressBar instanceof HTMLElement) {
-                              progressBar.style.display = "block";
-                              const realProgressBar =
-                                progressBar.firstElementChild.firstElementChild;
-                              if (challenge.type === "diet") {
-                                realProgressBar.value = result.length;
-                                realProgressBar.max = challenge.diet.condition;
-                                const span = realProgressBar.nextElementSibling;
-                                span.innerText =
-                                  Math.round(
-                                    (result.length / challenge.diet.condition) *
-                                      100
-                                  ) + " %";
-                              } else {
-                                realProgressBar.value = result.length;
-                                realProgressBar.max =
-                                  challenge.recipe.uploadCount;
-                                const span = realProgressBar.nextElementSibling;
-                                span.innerText =
-                                  Math.round(
-                                    (result.length /
-                                      challenge.recipe.uploadCount) *
-                                      100
-                                  ) + " %";
-                              }
-                            }
-                          } else {
-                            // 성공했다.
-                            button.textContent = "성공!";
-                            button.style.backgroundColor = "blue";
-
-                            setInterval(
-                              (button) => {
-                                button.style.display = "none";
-                              },
-                              2000,
-                              button
-                            );
-                          }
+                      </ul>
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          backgroundColor: "rgb(255,147,147)",
                         }}
                       >
-                        챌린지 결과 확인
-                      </button>
-                      <div style={{ display: "none" }}>
-                        <ProgressBar />
+                        <button
+                          className={ButtonStyle.button1}
+                          onClick={async (event) => {
+                            const { data: result } = await axios.post(
+                              "/api/challenge/validate",
+                              challenge
+                            );
+                            console.log(result);
+                            const button = event.target;
+
+                            button.disabled = true;
+                            if (Array.isArray(result)) {
+                              // 실패했다.
+                              button.textContent = "진행중!";
+                              button.style.color = "white";
+                              setInterval(
+                                (button) => {
+                                  button.style.display = "none";
+                                },
+                                2000,
+                                button
+                              );
+                              const progressBar = button.nextElementSibling;
+                              if (progressBar instanceof HTMLElement) {
+                                progressBar.style.display = "block";
+                                const realProgressBar =
+                                  progressBar.firstElementChild
+                                    .firstElementChild;
+                                if (challenge.type === "diet") {
+                                  realProgressBar.value = result.length;
+                                  realProgressBar.max =
+                                    challenge.diet.condition;
+                                  const span =
+                                    realProgressBar.nextElementSibling;
+                                  span.innerText =
+                                    Math.round(
+                                      (result.length /
+                                        challenge.diet.condition) *
+                                        100
+                                    ) + " %";
+                                } else {
+                                  realProgressBar.value = result.length;
+                                  realProgressBar.max =
+                                    challenge.recipe.uploadCount;
+                                  const span =
+                                    realProgressBar.nextElementSibling;
+                                  span.innerText =
+                                    Math.round(
+                                      (result.length /
+                                        challenge.recipe.uploadCount) *
+                                        100
+                                    ) + " %";
+                                }
+                              }
+                            } else {
+                              // 성공했다.
+                              button.textContent = "성공!";
+                              button.style.backgroundColor = "blue";
+
+                              setInterval(
+                                (button) => {
+                                  button.style.display = "none";
+                                },
+                                2000,
+                                button
+                              );
+                            }
+                          }}
+                        >
+                          챌린지 결과 확인
+                        </button>
+                        <div style={{ display: "none" }}>
+                          <ProgressBar />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </>
-              ) : null}
-            </>
-          );
-        })}
+                  </>
+                ) : null}
+              </>
+            );
+          })}
+        </div>
       </div>
     </>
   );
