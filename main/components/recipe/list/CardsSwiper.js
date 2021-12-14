922184332f8289fcbd3a0960269e55051494dc00
kanglaeheon <kanglaeheon@gmail.com>
2021-12-14 15:56:50 +0900

    카드에 detail 버튼 추가, hover에서 detail 버튼 조작시 내용 조회 가능하게 수정, 모바일 Swipe {true} 변경 및 prev,next 컴포넌트 SwiperNavation으로 분리

--- main/components/recipe/list/CardsSwiper.js
+++ main/components/recipe/list/CardsSwiper.js
@@ -1,11 +1,10 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/dist/client/image";
 
 // swiper
 import { Swiper, SwiperSlide, user } from "swiper/react";
 import "swiper/swiper.min.css";
-import "swiper/components/navigation/navigation.min.css";
 import SwiperCore, { Navigation } from "swiper/core";
 SwiperCore.use([Navigation]);
 
@@ -28,6 +27,7 @@ const CardsSwiper = ({ filteredHitRecipesProps, user }) => {
   const [filteredHitRecipes, setFilteredHitRecipes] = useState(
     filteredHitRecipesProps
   );
+  const [isDetailActive, setIsDetailActive] = useState(false);
   function renderSwitchCategory(param) {
     switch (param) {
       case "soup":
@@ -51,49 +51,35 @@ const CardsSwiper = ({ filteredHitRecipesProps, user }) => {
     }
   }
 
-  const navigationPrevRef = useRef(null);
-  const navigationNextRef = useRef(null);
-
   const swiperParams = {
-    navigation: {
-      prevEl: navigationPrevRef.current,
-      nextEl: navigationNextRef.current,
-    },
-    // IE, Edge, Firefox(>5) 이벤트 처리용 핸들러
-    onBeforePrint: (swiper) => {
-      swiper.params.navigation.prevEl = navigationPrevRef.current;
-      swiper.params.navigation.nextEl = navigationNextRef.current;
-      swiper.activeIndex = mainImageIndex;
-      swiper.navigation.update();
-    },
-    allowTouchMove: false,
     speed: 400,
     slidesPerView: 1.3,
     centeredSlides: true,
     loop: true,
-    preventInteractionOnTransition: true,
     spaceBetween: 30,
     onSwiper: setSwiper,
     onSlideChange: (e) => setMainImageIndex(e.activeIndex),
   };
 
+  useEffect(() => {
+    setIsDetailActive(false);
+  }, [mainImageIndex]);
   return (
     <Swiper
       {...swiperParams}
       ref={setSwiper}
       className={cardsSwiperStyles.container}
     >
-      <div className={cardsSwiperStyles.nvWrapper}>
-        <div className={cardsSwiperStyles.btnWrapper} ref={navigationPrevRef}>
-          <div className={cardsSwiperStyles.btnPrev}></div>
-        </div>
-        <div className={cardsSwiperStyles.btnWrapper} ref={navigationNextRef}>
-          <div className={cardsSwiperStyles.btnNext}></div>
-        </div>
-      </div>
       {filteredHitRecipes.map((card, index) => {
         return (
-          <SwiperSlide key={card._id} className={cardsSwiperStyles.card}>
+          <SwiperSlide
+            key={card._id}
+            className={
+              isDetailActive
+                ? cardsSwiperStyles.cardClick
+                : cardsSwiperStyles.card
+            }
+          >
             {/* 카드 헤더 (이미지) */}
             <div className={cardsSwiperStyles.cardHeader}>
               <Image
@@ -131,10 +117,28 @@ const CardsSwiper = ({ filteredHitRecipesProps, user }) => {
               <div className={cardsSwiperStyles.cardBodyHeader}>
                 <div className={cardsSwiperStyles.headerFlex}>
                   <div className={cardsSwiperStyles.hot}>
-                    <div className={cardsSwiperStyles.iWrapper}>
-                      <Image objectFit="contain" src={ci} />
+                    <div className={cardsSwiperStyles.innerHotWrapper}>
+                      <div className={cardsSwiperStyles.iWrapper}>
+                        <Image
+                          layout="intrinsic"
+                          objectFit="contain"
+                          src={ci}
+                        />
+                      </div>
+                      <strong>요즘 뜨는 레시피!</strong>
                     </div>
-                    <strong>요즘 뜨는 레시피</strong>
+
+                    <div className={cardsSwiperStyles.detailBtn}>
+                      {isDetailActive ? (
+                        <span onClick={() => setIsDetailActive(false)}>
+                          접기<i className="caret up icon"></i>
+                        </span>
+                      ) : (
+                        <span onClick={() => setIsDetailActive(true)}>
+                          자세히보기<i className="caret down icon"></i>
+                        </span>
+                      )}
+                    </div>
                   </div>
                 </div>
 
