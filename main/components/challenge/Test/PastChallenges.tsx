9ceb17942db3b195a248d57e53e383934279203d
박지훈 <qkrw6@naver.com>
2021-12-20 17:42:09 +0900

    main challenge

--- main/components/challenge/Test/PastChallenges.tsx
+++ main/components/challenge/Test/PastChallenges.tsx
@@ -9,18 +9,48 @@ interface Props {
   userId?: number
 }
 
-const PastChallenge = (challenge: Challenge) => (
-  <div style={{ display:"flex", justifyContent:"space-between"}} key={challenge._id}>
-    <div>
+const PastChallenge = (challenge: Challenge ) => (
+  <div style={{display:"flex", justifyContent:"center"}} key={challenge._id}>
+    <div className={PastStyles.imageDiv} >
     <Image
      className={PastStyles.image}
-    src={process.env.NEXT_PUBLIC_STATIC_SERVER_URL + challenge.image}
+    src={process.env.NEXT_PUBLIC_STATIC_SERVER_URL as string + challenge.image}
     />
-    {challenge.title}
-    {challenge.startDate}~{challenge.endDate}
-    {challenge.hasOwnProperty("result") ? "실패" : "성공"}
     </div>
-  </div>)
+    <div className={PastStyles.ulDiv}>
+      <ul>
+      <li style={{font:"normal 600 1.2rem/22px Noto Sans KR"}}> 
+      {challenge.title}</li>
+      <li style={{font:"normal 500 1rem/22px Noto Sans KR"}}> {challenge.startDate?.getFullYear()+"."+ 
+           (challenge.startDate?.getMonth() as number+1)+"."
+           +challenge.startDate?.getDate()}
+           ~{challenge.endDate?.getFullYear()+"."
+           + (challenge.endDate?.getMonth() as number+1)+"."
+           +challenge.endDate?.getDate()}
+     </li>
+     <br/>
+     <li> {challenge.hasOwnProperty("result") ? <div 
+     style={{
+       border:"1px solid",
+       borderRadius:"0.3rem",
+       backgroundColor:"#fff5f5",
+       font: "normal 600 1.2rem/22px Noto sans KR",
+       color:"#F15F5F",
+       width:"80%",
+     }}
+     >최종달성율 {challenge.result}% </div> :<div
+     style={{
+      border:"1px solid",
+      borderRadius:"0.3rem",
+      backgroundColor:"#fff5f5",
+      font: "normal 600 1.2rem/22px Noto sans KR",
+      color:"#F15F5F",
+      width:"90%",
+    }}>최종달성율 100%</div>}</li>
+     </ul>
+    </div>
+  </div>
+  )   
 
 
 const PastChallenges: React.FC<Props> = () => {
@@ -28,7 +58,10 @@ const PastChallenges: React.FC<Props> = () => {
   const handleClick = async () => {
     try {
       const { data: { challenges: pastChallenges } } = await axios.get('/api/challenge/past');
-      setChallenges(pastChallenges);
+      setChallenges(pastChallenges.map((challenge:any) => {
+        
+        return {...challenge, startDate: new Date(challenge.startDate), endDate: new Date(challenge.endDate)}
+      }));
 
     } catch (error) {
       console.error(error);
