286af7708a0e9f305916a2b4051e35a6d90c30b5
Minsung Kim <epicblue@hanmail.net>
2021-12-22 17:49:27 +0900

    색상 상수 저장

--- main/constants/index.ts
+++ main/constants/index.ts
@@ -1,13 +1,17 @@
 import { CSSProperties } from "react";
 
-export const MAIN_COLOR = "black";
+export const MAIN_COLOR = "#FF5656";
+export const BORDER_COLOR = "#ddd";
+export const MAIN_FONT_COLOR = "#333";
+
+export const MAIN_CONTAINER_PADDING = "1rem";
+export const DEFAULT_GAP_MARGIN = "1rem";
+
+// 나중에
 
 export const BACKGROUND_COLOR = "white";
 export const MIDDLE_COLOR = "#ff9393";
 export const FLEXBOX_NORMAL: CSSProperties = {
-  // display: "flex",
-  // justifyContent: "space-between",
-  display: "grid",
-  gridTemplateColumns: "4fr 6fr",
-  gap: "1rem"
+  display: "flex",
+  justifyContent: "space-between",
 };
