f2671e118d21c6d10023ba3bfebc5197e0367c14
Minsung Kim <epicblue@hanmail.net>
2022-02-17 13:18:09 +0900

    Loading 컴포넌트 addDots 함수 리팩터링

--- main/components/main/Loading.tsx
+++ main/components/main/Loading.tsx
@@ -5,28 +5,25 @@ import mainStyle from '../../styles/main/Main.module.css';
 
 const comments = ["간식 참는 중", "야채 사러 가는 중", "라면 다시 집어 넣는 중"]
 
-const addDots = (textArea: HTMLDivElement) =>
-  new Promise<HTMLDivElement>((resolve, reject) => {
-    setTimeout(() => {
-      if (!textArea) reject(new Error("no element"))
-      textArea.textContent += ".";
-      resolve(textArea);
-    }, 150)
-  })
+const addDots = async (textArea: Element, numberOfDots: number, ms: number) => {
+  if (numberOfDots === 0) return;
+  setTimeout(() => {
+    if (!textArea) throw new Error("no element")
+    textArea.textContent += ".";
+    addDots(textArea, numberOfDots - 1, ms);
+
+  }, ms)
+
+}
 
 const Loading = () => {
   const comment = comments[Math.floor(Math.random() * comments.length)];
   useEffect(() => {
-    const textArea = loadingTheme.current.children[1] as HTMLDivElement;
-    addDots(textArea)
-      .then(addDots)
-      .then(addDots)
-      .then(addDots)
-      .then(addDots)
-      .then(addDots)
-      .catch();
+    const textArea = loadingTheme.current!.children[1]
+    addDots(textArea, 6, 150)
+
     const timer1 = setTimeout(() => {
-      loadingTheme.current.style.opacity = "0"
+      loadingTheme.current!.style.opacity = "0"
       setTimeout(() => {
         if (loadingTheme.current)
           loadingTheme.current.style.display = "none"
@@ -38,7 +35,7 @@ const Loading = () => {
     }
   }, [])
 
-  const loadingTheme = useRef() as MutableRefObject<HTMLDivElement>
+  const loadingTheme = useRef<HTMLDivElement>(null)
   return (
     <div className={mainStyle.loadingTheme} ref={loadingTheme}>
       <div className={mainStyle.loadingCircle} />
